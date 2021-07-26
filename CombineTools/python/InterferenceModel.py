1bbfed80c7d1b90df93a7ffe5f673e1f57c6e977
Afiq Anuar <afiq.anuar@cern.ch>
2021-07-26 23:18:52 +0200

    minor syntax change to match datacard script

--- CombineTools/python/InterferenceModel.py
+++ CombineTools/python/InterferenceModel.py
@@ -13,20 +13,20 @@ class InterferenceModel(PhysicsModel):
         self.modelBuilder.factory_('expr::r_neg_sq("(-@0*@0)", r)')
         self.modelBuilder.factory_('expr::r_neg_tothe4("(-@0*@0*@0*@0)", r)')
         self.modelBuilder.factory_('expr::r_tothe4("(@0*@0*@0*@0)", r)')
-        
+
     def getYieldScale(self, bin, process):
         "Return the name of a RooAbsReal to scale the yield"
         if not self.DC.isSignal[process]:
             return 1
         if '_neg' in process:
-            if '-sgn' in process:
+            if '_res' in process:
                 print 'Scaling', process, 'with negative signal strength to the 4'
                 print 'WARNING, negative signal part, sure this is what you want?'
                 return 'r_neg_tothe4'
             else:
                 print 'Scaling', process, 'with negative signal strength squared'
                 return 'r_neg_sq'
-        elif '-sgn' in process:
+        elif '_res' in process:
             print 'Scaling', process, 'with signal strength to the 4'
             return 'r_tothe4'
         print 'Scaling', process, 'with signal strength squared'
--- CombineTools/python/InterferencePlusFixed.py
+++ CombineTools/python/InterferencePlusFixed.py
@@ -14,20 +14,20 @@ class InterferencePlusFixed(PhysicsModel):
         self.modelBuilder.factory_('expr::g_neg_sq("(-@0*@0)*@1", g, r)')
         self.modelBuilder.factory_('expr::g_neg_tothe4("(-@0*@0*@0*@0)*@1", g, r)')
         self.modelBuilder.factory_('expr::g_tothe4("(@0*@0*@0*@0)*@1", g, r)')
-        
+
     def getYieldScale(self, bin, process):
         "Return the name of a RooAbsReal to scale the yield"
         if not self.DC.isSignal[process]:
             return 1
         if '_neg' in process:
-            if '-sgn' in process:
+            if '_res' in process:
                 print 'Scaling', process, 'with negative coupling modifier to the 4'
                 print 'WARNING, negative signal part, sure this is what you want?'
                 return 'g_neg_tothe4'
             else:
                 print 'Scaling', process, 'with negative coupling modifier squared'
                 return 'g_neg_sq'
-        elif '-sgn' in process:
+        elif '_res' in process:
             print 'Scaling', process, 'with coupling modifier to the 4'
             return 'g_tothe4'
         print 'Scaling', process, 'with coupling modifier squared'
