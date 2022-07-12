9f79a2632b7e771b1bd45b3ee0770e42353b1ea3
Afiq Anuar <afiqaize@gmail.com>
2022-07-12 15:56:44 +0200

    LO fc contour taking form

--- ahtt/scripts/plot_contour.py
+++ ahtt/scripts/plot_contour.py
@@ -31,16 +31,86 @@ def read_contour(cfiles):
         contours[ii]["g1"] = []
         contours[ii]["g2"] = []
         contours[ii]["eff"] = []
+        contours[ii]["avg"] = 0
 
         for gv in cc["g-grid"].keys():
             contours[ii]["g1"].append( gv.replace(" ", "").split(",")[0] )
             contours[ii]["g2"].append( gv.replace(" ", "").split(",")[1] )
             contours[ii]["eff"].append( cc["g-grid"][gv]["pass"] / cc["g-grid"][gv]["total"] )
+            contours[ii]["avg"] += cc["g-grid"][gv]["total"]
+
+        contours[ii]["avg"] /= len(contours[ii]["eff"])
 
     return contours
 
-def draw_contour(ofile, pair, contours, labels, maxsigma, transparent):
-    pass
+def draw_contour(oname, pair, cfiles, labels, maxsigma, transparent):
+    contours = read_contour(cfiles)
+    ncontour = len(contours)
+    alphas = [1 - pval for pval in [0.6827, 0.9545, 0.9973, 0.999937, 0.9999997]]
+
+    if not hasattr(draw_contour, "colors"):
+        draw_1D.colors = OrderedDict([
+            (1    , ["0"]),
+            (2    , ["#cc0033", "#0033cc"]),
+            (3    , ["0", "#cc0033", "#0033cc"]),
+            (4    , ["0", "#cc0033", "#0033cc", "#33cc00"]),
+        ])
+        draw_1D.lines = ['solid', 'dashed', 'dashdot', 'dashdotdotted', 'dotted']
+
+
+    fig, ax = plt.subplots()
+    handles = []
+    sigmas = []
+
+    for ic, contour in enumerate(contours):
+        xv, yv = np.meshgrid(np.array(set(contour["g1"])), np.array(set(contour["g2"])))
+        zv = np.zeros_like(xv)
+
+        for ir, xr in enumerate(xv):
+            for ic, xc in enumerate(xr):
+                g1 = xv[ir][ic]
+                g2 = yv[ir][ic]
+
+                for i1, i2, ie in zip(contour["g1"], contour["g2"], contour["eff"]):
+                    if i1 == g1 and i2 == g2:
+                        zv[ir][ic] = ie
+                        break
+
+        for isig in range(maxsigma):
+            if ic == 0 and maxsigma > 1:
+                sigmas.append((mln.Line2D([0], [0], color = "0", linestyle = draw_1D.lines[isig], linewidth = 2), r"$\pm" + str(isig + 1) + r"\sigma$"))
+
+            alpha = alphas[isig]
+
+            if contour["avg"] < (4.5 / alpha):
+                print("average toy count of " + str(contour["avg"]) + " likely insufficient to determine contour with CL " + str(alpha) + "\n")
+
+            cf = ax.contourf(xv, yv, zv, [-1., alpha], colors = ["#ffffff"], alpha = 0.)
+            ax.contour(cf, colors = draw_1D.colors[len(contours)][ic], linestyles = draw_1D.lines[isig], linewidths = 2, alpha = 1. - (0.05 * isig))
+
+            if len(labels) > 1 and isig == 0:
+                handles.append((mln.Line2D([0], [0], color = draw_1D.colors[len(contours)][ic], linestyle = solid, linewidth = 2), labels[ic]))
+
+    plt.xlabel(axes["coupling"] % get_point(pair[0])[0], fontsize = 21, loc = "right")
+    plt.ylabel(axes["coupling"] % get_point(pair[1])[0], fontsize = 21, loc = "top")
+    ax.margins(x = 0, y = 0)
+
+    if len(handles) > 0 and len(sigmas) > 0:
+        pass
+    elif len(handles) > 0:
+        pass
+    elif len(sigmas) > 0:
+        pass
+
+    ax.minorticks_on()
+    ax.tick_params(axis = "both", which = "both", direction = "in", bottom = True, top = False, left = True, right = True)
+    ax.tick_params(axis = "both", which = "major", width = 1, length = 8, labelsize = 18)
+    ax.tick_params(axis = "both", which = "minor", width = 1, length = 3)
+
+    fig.set_size_inches(8., 8.)
+    fig.tight_layout()
+    fig.savefig(oname, transparent = transparent)
+    fig.clf()
 
 if __name__ == '__main__':
     parser = ArgumentParser()
