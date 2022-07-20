d2394c6b8a4b5eb6c1cf3323810d94cc64dad81c
Afiq Anuar <afiqaize@gmail.com>
2022-07-20 17:09:26 +0200

    drawing stuff

--- ahtt/scripts/plot_contour.py
+++ ahtt/scripts/plot_contour.py
@@ -86,10 +86,7 @@ def draw_contour(oname, pair, cfiles, labels, maxsigma, scatter, formal, cmsapp,
                 handles.append((mln.Line2D([0], [0], color = draw_contour.colors[len(contours)][ic], linestyle = 'solid', linewidth = 2), labels[ic]))
 
         if scatter:
-            sc = ax.plot(np.array(contour["g1"]), np.array(contour["g2"]), marker = '.', ls = '', lw = 0., color = draw_contour.colors[len(contours)][ic], alpha = 0.5)
-            sc.remove()
-            ax.relim()
-            ax.add_collection(sc, autolim = False)
+            ax.plot(np.array(contour["g1"]), np.array(contour["g2"]), marker = '.', ls = '', lw = 0., color = draw_contour.colors[len(contours)][ic], alpha = 0.5, autolim = False)
 
     plt.xlabel(axes["coupling"] % str_point(pair[0]), fontsize = 23, loc = "right")
     plt.ylabel(axes["coupling"] % str_point(pair[1]), fontsize = 23, loc = "top")
