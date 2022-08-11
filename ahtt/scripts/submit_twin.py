7e466655f5f43fe37691fd49df9af177e4c0ef93
Afiq Anuar <afiqaize@gmail.com>
2022-08-11 07:46:09 +0200

    oops. good thing this is almost identical as conservative atm

--- ahtt/scripts/submit_twin.py
+++ ahtt/scripts/submit_twin.py
@@ -169,7 +169,7 @@ if __name__ == '__main__':
                         "exp-b -> g1 = g2 = 0; exp-s -> g1 = g2 = 1; exp-01 -> g1 = 0, g2 = 1; exp-10 -> g1 = 1, g2 = 0",
                         default = "exp-b", dest = "fcexp", required = False)
     parser.add_argument("--fc-nuisance-mode", help = "how to handle nuisance parameters in toy generation (see https://arxiv.org/abs/2207.14353)",
-                        default = "profile", dest = "fcnui", required = False)
+                        default = "conservative", dest = "fcnui", required = False)
     parser.add_argument("--fc-n-toy", help = "number of toys to throw per FC grid scan",
                         default = 175, dest = "fctoy", required = False, type = int)
     parser.add_argument("--fc-skip-data", help = "skip running on data/asimov", dest = "fcrundat", action = "store_false", required = False)
