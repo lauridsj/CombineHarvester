1fa361e1c43573f113eada558e0e79b1160d1e37
Afiq Anuar <afiqaize@gmail.com>
2022-08-24 14:47:06 +0200

    syntax for drawing multiple contours

--- ahtt/scripts/plot_contour.py
+++ ahtt/scripts/plot_contour.py
@@ -147,13 +147,26 @@ def draw_contour(oname, pair, cfiles, labels, maxsigma, propersig, drawcontour, 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("--contour", help = "the json files containing the contour information, semicolon separated. must be of the same signal pair.", default = "", required = True)
+    parser.add_argument("--point",
+                        help = "desired pairs of signal points to run on, comma (between points) and semicolon (between pairs) separated\n"
+                        "another syntax is: m1,m2,...,mN;w1,w2,...,wN;m1,m2,...,mN;w1,w2,...,wN, where:\n"
+                        "the first mass and width strings refer to the A grid, and the second to the H grid.\n"
+                        "both mass and width strings must include their m and w prefix, and for width, their p0 suffix.\n"
+                        "e.g. m400,m450;w5p0;m750;w2p5,w10p0 expands to A_m400_w5p0,H_m750_w2p5;A_m400_w5p0,H_m750_w10p0;A_m450_w5p0,H_m750_w2p5;A_m450_w5p0,H_m750_w10p0",
+                        default = "", required = False)
+    parser.add_argument("--contour", help = "the json files containing the contour information, semicolon separated.\n"
+                        "two separate syntaxes are possible:\n"
+                        "'latest': t1/s1,s2;t2/s3... expands to <pnt>_<t1>/<pnt>_fc_scan_<s1>.json;<pnt>_<t1>/<pnt>_fc_scan_<s2>.json;<pnt>_<t2>/<pnt>_fc_scan_<s3>.json, "
+                        " where the code will search for the latest indices corresponding to scenario s1 and so on. used if --point is non-empty, and looped over all pairs. \n"
+                        "'direct': <json 1>;<json 2>;... used only when --point is empty",
+                        default = "", required = True)
+
     parser.add_argument("--otag", help = "extra tag to append to plot names", default = "", required = False)
     parser.add_argument("--odir", help = "output directory to dump plots in", default = ".", required = False)
     parser.add_argument("--max-sigma", help = "max number of sigmas to be drawn on the contour", dest = "maxsigma", default = 2, type = int, required = False)
     parser.add_argument("--label", help = "labels to attach on plot for each json input, semicolon separated", default = "", required = False)
 
-    parser.add_argument("--draw-scatter", help = "draw the scatter points used to build the contours. matplotlib is insane, so this feature doesn't actually work!",
+    parser.add_argument("--draw-scatter", help = "draw the scatter points used to build the contours",
                         dest = "scatter", action = "store_true", required = False)
     parser.add_argument("--draw-best-fit", help = "draw the best fit point.",
                         dest = "bestfit", action = "store_true", required = False)
@@ -178,17 +191,60 @@ if __name__ == '__main__':
     if (args.fmt != "" and not args.fmt.startswith(".")):
         args.fmt = "." + args.fmt
 
-    contours = args.contour.split(';')
     labels = args.label.split(';')
 
-    if len(contours) != len(labels):
-        raise RuntimeError("there aren't as many input contours as there are labels. aborting")
+    if args.point != "":
+        pairs = args.point.replace(" ", "").split(';')
 
-    pairs = [os.path.basename(os.path.dirname(cc)).split(".")[0].split("__") for cc in contours]
-    pairs = [[pp[0], "_".join(pp[1].split("_")[:3])] for pp in pairs]
+        # handle the case of gridding pairs
+        if len(pairs) == 4:
+            pairgrid = [pp.split(",") for pp in pairs]
+            if all([mm.startswith("m") for mm in pairgrid[0] + pairgrid[2]]) and all([ww.startswith("w") for ww in pairgrid[1] + pairgrid[3]]):
+                alla = []
+                for mm in pairgrid[0]:
+                    for ww in pairgrid[1]:
+                        alla.append("_".join(["A", mm, ww]))
 
-    if not all([pp == pairs[0] for pp in pairs]):
-        raise RuntimeError("provided contours are not all of the same pair of points!!")
+                allh = []
+                for mm in pairgrid[2]:
+                    for ww in pairgrid[3]:
+                        allh.append("_".join(["H", mm, ww]))
 
-    draw_contour("{ooo}/{prs}_fc-contour{tag}{fmt}".format(ooo = args.odir, prs = "__".join(pairs[0]), tag = args.otag, fmt = args.fmt), pairs[0], contours, labels, args.maxsigma, args.propersig,
-                 args.drawcontour, args.bestfit, args.scatter, args.formal, args.cmsapp, args.luminosity, args.transparent)
+                pairs = []
+                for aa in alla:
+                    for hh in allh:
+                        pairs.append([aa, hh])
+    else:
+        pairs = [os.path.basename(os.path.dirname(cc)).split(".")[0].split("__") for cc in contours]
+        pairs = [[pp[0], "_".join(pp[1].split("_")[:3])] for pp in pairs]
+
+        if not all([pp == pairs[0] for pp in pairs]):
+            raise RuntimeError("provided contours are not all of the same pair of points!!")
+
+        pairs = [pairs[0]]
+        contours = args.contour.split(';')
+
+    for pair in pairs:
+        pstr = "__".join(pair)
+
+        if args.point != "":
+            contour = []
+            tags = args.contour.replace(" ", "").split(';')
+            tags = [tt for tt in tags if tt.startswith("_") else "_" + tt]
+
+            for tag in tags:
+                fcexps = tag.split('/')[1].split(',')
+                for fcexp in fcexps:
+                    ggg = glob.glob(pstr + tag + "/" + pstr + "_fc_scan_" + fcexp + "_*.json")
+                    ggg.sort(key = os.path.getmtime)
+                    contour.append(ggg[-1])
+        else:
+            contour = contours
+
+        if len(contour) != len(labels):
+            raise RuntimeError("there aren't as many input contours as there are labels. aborting")
+
+        print("drawing contours for pair: ", pair)
+        print("using the following contours: ", contour)
+        draw_contour("{ooo}/{prs}_fc-contour{tag}{fmt}".format(ooo = args.odir, prs = pstr, tag = args.otag, fmt = args.fmt), pair, contour, labels,
+                     args.maxsigma, args.propersig, args.drawcontour, args.bestfit, args.scatter, args.formal, args.cmsapp, args.luminosity, args.transparent)
