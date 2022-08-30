fe01e22cc8c761925efe0002f0dddc7cde922e13
Afiq Anuar <afiqaize@gmail.com>
2022-08-30 15:25:03 +0200

    local run log

--- ahtt/scripts/utilities.py
+++ ahtt/scripts/utilities.py
@@ -188,7 +188,7 @@ def submit_job(job_agg, job_name, job_arg, job_time, job_cpu, job_mem, job_dir, 
         submit_job.firstprint = True
 
     if runlocal:
-        syscall('{executable} {job_arg}'.format(executable = executable, job_arg = job_arg), True)
+        syscall('{executable} {job_arg} >& {log}.olocal.1'.format(executable = executable, job_arg = job_arg, log = job_dir + '/' + job_name), True)
     else:
         syscall('{csub} -s {cpar} -w {crun} -n {name} -e {executable} -a "{job_arg}" {job_time} {job_cpu} {tmp} {job_dir} --debug'.format(
             csub = condordir + "condorSubmit.sh",
