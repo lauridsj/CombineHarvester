faf743ee7176ab26e11844606136e988407f4404
Afiq Anuar <afiqaize@gmail.com>
2022-08-31 15:14:56 +0200

    gah curve

--- ahtt/scripts/plot_limit.py
+++ ahtt/scripts/plot_limit.py
@@ -338,6 +338,11 @@ def draw_1D(oname, limits, labels, xaxis, yaxis, ltitle, gcurve, drawband, obser
         fixed_value = float(fixed_value.replace('m', '').replace('w', '').replace('p', '.'))
 
         max_partial_g = [ahtt_max_coupling(parity, fixed_value, xx) if ismass else ahtt_max_coupling(parity, xx, fixed_value / 100.) for xx in xvalues]
+
+        if fixed_value == 8.:
+            print(xmaxg)
+            print(max_partial_g)
+
         max_partial_g = [gg for gg in max_partial_g if gg < ymax1]
         xmaxg = [xx for gg, xx in zip(max_partial_g, xvalues) if gg < ymax1]
         may_partial_g = [min(gg + 0.05 * (ymax1 - ymin), ymax1) for gg in max_partial_g]
