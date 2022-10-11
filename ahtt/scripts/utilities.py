dae5ebe108ad8d42d3265ef983c18c908083204c
Afiq Anuar <afiqaize@gmail.com>
2022-10-11 11:51:51 +0200

    debug get_nbin

--- ahtt/scripts/utilities.py
+++ ahtt/scripts/utilities.py
@@ -124,8 +124,7 @@ def get_nbin(fname, channel, year):
     hfile = TFile.Open(fname, "read")
     hfile.cd(channel + "_" + year)
     keys = gDirectory.GetListOfKeys()
-    print keys[0]
-    histogram = keys[0].ReadObj()
+    histogram = keys[-1].ReadObj()
     nbin = histogram.GetNbinsX()
     hfile.Close()
     return nbin
