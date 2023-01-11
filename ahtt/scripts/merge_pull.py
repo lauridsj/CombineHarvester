c9ea0af32e5b40c0a400559060348c4e0ed0870f
Afiq Anuar <afiqaize@gmail.com>
2023-01-11 21:10:50 +0100

    allow impact grouping

--- ahtt/scripts/merge_pull.py
+++ ahtt/scripts/merge_pull.py
@@ -12,35 +12,12 @@ import json
 
 from desalinator import prepend_if_not_empty, tokenize_to_list, remove_spaces_quotes
 
-def dump_pull(directories, onepoi, gvalue, rvalue, fixpoi):
+def dump_pull(directories, onepoi, gvalue, rvalue, fixpoi, nuisances, otag):
     for directory, tag in directories:
         pulls = OrderedDict()
-        impacts = glob.glob("{dcd}/{pnt}_{tag}_impacts_{mod}{gvl}{rvl}{fix}*.json".format(
-            dcd = directory,
-            tag = tag,
-            pnt = '_'.join(directory.split('_')[:3]),
-            mod = "one-poi" if onepoi else "g-scan",
-            gvl = "_g_" + str(gvalue).replace(".", "p") if gvalue >= 0. else "",
-            rvl = "_r_" + str(rvalue).replace(".", "p") if rvalue >= 0. and not onepoi else "",
-            fix = "_fixed" if fixpoi and (gvalue >= 0. or rvalue >= 0.) else "",
-        ))
 
-        for ii, imp in enumerate(impacts):
-            print("merge_pull :: opening file " + imp)
-            with open(imp) as ff:
-                result = json.load(ff)
-
-            if ii == 0:
-                pulls["POIs"] = result["POIs"]
-                pulls["method"] = result["method"]
-                pulls["params"] = result["params"]
-            else:
-                if pulls["POIs"] != result["POIs"]:
-                    print("merge_pull :: WARNING :: incompatible POI best fit between input jsons!!")
-
-                pulls["params"] += result["params"]
-
-        with open("{dcd}/{pnt}_{tag}_impacts_{mod}{gvl}{rvl}{fix}_all.json".format(
+        for group in ["expth", "mcstat"]:
+            impacts = glob.glob("{dcd}/{pnt}_{tag}_impacts_{mod}{gvl}{rvl}{fix}_{grp}_*.json".format(
                 dcd = directory,
                 tag = tag,
                 pnt = '_'.join(directory.split('_')[:3]),
@@ -48,6 +25,36 @@ def dump_pull(directories, onepoi, gvalue, rvalue, fixpoi):
                 gvl = "_g_" + str(gvalue).replace(".", "p") if gvalue >= 0. else "",
                 rvl = "_r_" + str(rvalue).replace(".", "p") if rvalue >= 0. and not onepoi else "",
                 fix = "_fixed" if fixpoi and (gvalue >= 0. or rvalue >= 0.) else "",
+                grp = group
+            ))
+
+            for ii, imp in enumerate(impacts):
+                print("merge_pull :: opening file " + imp)
+                with open(imp) as ff:
+                    result = json.load(ff)
+
+                if len(nuisances) > 0:
+                    result["params"] = [param for param in result["params"] if nuisance in param["name"] for nuisance in nuisances]
+
+                if ii == 0:
+                    pulls["POIs"] = result["POIs"]
+                    pulls["method"] = result["method"]
+                    pulls["params"] = result["params"]
+                else:
+                    if pulls["POIs"] != result["POIs"]:
+                        print("merge_pull :: WARNING :: incompatible POI best fit between input jsons!!")
+
+                    pulls["params"] += result["params"]
+
+        with open("{dcd}/{pnt}_{tag}_impacts_{mod}{gvl}{rvl}{fix}_{out}.json".format(
+                dcd = directory,
+                tag = tag,
+                pnt = '_'.join(directory.split('_')[:3]),
+                mod = "one-poi" if onepoi else "g-scan",
+                gvl = "_g_" + str(gvalue).replace(".", "p") if gvalue >= 0. else "",
+                rvl = "_r_" + str(rvalue).replace(".", "p") if rvalue >= 0. and not onepoi else "",
+                fix = "_fixed" if fixpoi and (gvalue >= 0. or rvalue >= 0.) else "",
+                out = otag
         ), "w") as jj:
             json.dump(pulls, jj, indent = 1)
 
@@ -59,6 +66,12 @@ if __name__ == '__main__':
     parser.add_argument("--tag", help = "input tag-output-tag pairs to search. the pairs are semicolon separated, and tags colon-separated, "
                         "so e.g. when there are 2 tags: 't1:o1;t2:o2...", dest = "itag", default = "", required = False, type = lambda s: tokenize_to_list( remove_spaces_quotes(s), ';' ))
     parser.add_argument("--one-poi", help = "plot pulls obtained with the g-only model", dest = "onepoi", action = "store_true", required = False)
+
+    parser.add_argument("--nuisances", help = "comma-separated list of nuisances to consider. greedy matching: XX,YY means any nuisances containing "
+                        "either XX or YY in the name is included."
+                        dest = "nuisance", default = "", required = False, type = lambda s: lambda s: [] if s == "" else tokenize_to_list( remove_spaces_quotes(s) ))
+    parser.add_argument("--impact-tag", help = "tag to attach to the merged impact json file"
+                        dest = "otag", default = "all", required = False)
 
     parser.add_argument("--g-value",
                         help = "g to use when evaluating impacts/fit diagnostics/nll. "
@@ -74,4 +87,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     dirs = [[args.point + '_' + tag.split(':')[0], tag.split(':')[1] if len(tag.split(':')) > 1 else tag.split(':')[0]] for tag in args.itag]
-    dump_pull(dirs, args.onepoi, args.setg, args.setr, args.fixpoi)
+    dump_pull(dirs, args.onepoi, args.setg, args.setr, args.fixpoi, args.nuisance, args.otag)
