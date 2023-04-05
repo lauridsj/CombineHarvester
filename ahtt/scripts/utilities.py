4bfd6d6087796179b4b38ced97f439759e96a26a
Afiq Anuar <afiqaize@gmail.com>
2023-04-05 13:02:46 +0200

    group stuff

--- ahtt/scripts/utilities.py
+++ ahtt/scripts/utilities.py
@@ -290,7 +290,7 @@ def submit_job(job_agg, job_name, job_arg, job_time, job_cpu, job_mem, job_dir, 
             # accounting shenanigans
             afiq_at_lxplus = "desy" not in input_base and os.getlogin() == 'afiqaize'
             if afiq_at_lxplus:
-                syscall("sed -i '/queue/i +AccountingGroup = group_u_CMST3.all' {agg}".format(agg = job_agg), False)
+                syscall("sed -i '/queue/i +AccountingGroup = \"group_u_CMST3.all\"' {agg}".format(agg = job_agg), False) # or group_u_CMS.u_zh.users
         else:
             syscall("echo >> {agg} && grep -F -x -v -f {agg} {name} >> {agg} && echo 'queue' >> {agg} && rm {name}".format(
                 name = 'conSub_' + job_name + '.txt',
