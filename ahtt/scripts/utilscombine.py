a3835a3ca96d1d1fe8a158f23becb7ed72290b17
Afiq Anuar <afiqaize@gmail.com>
2023-04-26 17:32:36 +0200

    require nonnegative dnll

--- ahtt/scripts/utilscombine.py
+++ ahtt/scripts/utilscombine.py
@@ -83,7 +83,7 @@ def read_nuisance(dname, points, qexp_eq_m1 = True):
     frzpar = []
 
     for i in dtree:
-        if (dtree.quantileExpected != -1. and qexp_eq_m1) or (dtree.quantileExpected == -1. and not qexp_eq_m1):
+        if dtree.deltaNLL < 0. or (dtree.quantileExpected != -1. and qexp_eq_m1) or (dtree.quantileExpected == -1. and not qexp_eq_m1):
             continue
 
         for nn in nuisances:
