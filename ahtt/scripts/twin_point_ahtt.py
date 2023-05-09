d17ea59a5344d8c88703b5383144b548191e00a4
Afiq Anuar <afiqaize@gmail.com>
2023-05-09 16:55:38 +0200

    propagate timestamp dir to merge

--- ahtt/scripts/twin_point_ahtt.py
+++ ahtt/scripts/twin_point_ahtt.py
@@ -13,7 +13,7 @@ import json
 
 from ROOT import TFile, TTree
 
-from utilspy import syscall, get_point, right_now, elementwise_add, tuplize, stringify
+from utilspy import syscall, recursive_glob, make_timestamp_dir, get_point, elementwise_add, tuplize, stringify, max_nfile_per_dir
 from utilscombine import read_nuisance, max_g, make_best_fit, starting_nuisance, fit_strategy, make_datacard_with_args, set_parameter
 
 from desalinator import prepend_if_not_empty, tokenize_to_list, remove_spaces_quotes
@@ -281,7 +281,7 @@ if __name__ == '__main__':
         if any(float(gg) < 0. for gg in gvalues):
             raise RuntimeError("in FC scans both g can't be negative!!")
 
-        if args.fcresdir = "":
+        if args.fcresdir == "":
             args.fcresdir = dcdir
 
         exp_scenario = OrderedDict()
@@ -370,15 +370,43 @@ if __name__ == '__main__':
                 ), False)
 
     if runhadd:
-        toys = glob.glob("{dcd}{pnt}{tag}_fc-scan_*_toys_*.root".format(dcd = dcdir, pnt = "__".join(points), tag = args.otag))
+        toys = recursive_glob(dcdir, "{pnt}{tag}_fc-scan_*_toys_*.root".format(pnt = "__".join(points), tag = args.otag))
 
         if len(toys) > 0:
             print "\ntwin_point_ahtt :: indexed toy files detected, merging them..."
-            toys = set([re.sub('toys_.*.root', 'toys.root', toy) for toy in toys])
+            toys = set([os.path.basename(re.sub('toys_.*.root', 'toys.root', toy)) for toy in toys])
 
-            for toy in toys:
-                syscall("mv {toy} {tox}".format(toy = toy, tox = toy.replace("toys.root", "toys_x.root")), False, True)
-                syscall("hadd {toy} {tox} && rm {tox}".format(toy = toy, tox = toy.replace("toys.root", "toys_*.root")))
+            mrgdir = ""
+            resdirs = []
+            for ii, toy in enumerate(toys):
+                if ii % max_nfile_per_dir == 0:
+                    mrgdir = make_timestamp_dir(base = dcdir, prefix = "fc-merge")
+
+                tomerge = recursive_glob(dcdir, toy)
+                if len(tomerge) > 0:
+                    syscall("mv {toy} {tox}".format(toy = tomerge[0], tox = toy.replace("toys.root", "toys_x.root")), False, True)
+
+                tomerge = recursive_glob(dcdir, toy.replace("toys.root", "toys_*.root"))
+                syscall("hadd {toy} {tox} && rm {tox}".format(toy = mrgdir + toy, tox = " ".join(tomerge)))
+
+                for tm in tomerge:
+                    if 'fc-result' in tm:
+                        resdirs.append(os.path.abspath(os.path.dirname(tm)))
+
+            for resdir in set(resdirs):
+                if len(recursive_glob(resdir, '*')) == 0:
+                    syscall("rm -r {rsd}".format(rsd = resdir))
+
+        #toys = glob.glob("{dcd}{pnt}{tag}_fc-scan_*_toys_*.root".format(dcd = dcdir, pnt = "__".join(points), tag = args.otag))
+
+        #if len(toys) > 0:
+        #    print "\ntwin_point_ahtt :: indexed toy files detected, merging them..."
+        #    toys = set([re.sub('toys_.*.root', 'toys.root', toy) for toy in toys])
+
+        #    for toy in toys:
+        #        # FIXME they now dont have to be in the same directory
+        #        syscall("mv {toy} {tox}".format(toy = toy, tox = toy.replace("toys.root", "toys_x.root")), False, True)
+        #        syscall("hadd {toy} {tox} && rm {tox}".format(toy = toy, tox = toy.replace("toys.root", "toys_*.root")))
 
     if runcompile:
         toys = glob.glob("{dcd}{pnt}{tag}_fc-scan_*_toys.root".format(dcd = dcdir, pnt = "__".join(points), tag = args.otag))
