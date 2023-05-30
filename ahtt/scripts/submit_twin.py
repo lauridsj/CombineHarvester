be88839106b357351eb64386d83d31411db95534
Afiq Anuar <afiqaize@gmail.com>
2023-05-30 16:48:37 +0200

    this level of typo smh

--- ahtt/scripts/submit_twin.py
+++ ahtt/scripts/submit_twin.py
@@ -305,7 +305,7 @@ if __name__ == '__main__':
                     jarg += " {toy} {idx} {opd}".format(
                         toy = "--n-toy " + str(args.ntoy) if args.ntoy > 0 else "",
                         idx = "--run-idx " + str(idx) if idx > -1 else "",
-                        opd = "--toy-location " + toylocs[ii] if toyloc != "" else ""
+                        opd = "--toy-location " + toylocs[ii] if toylocs[ii] != "" else ""
                     )
 
                     submit_job(agg, jname, jarg, args.jobtime, 1, "",
