988ee330737a8a94941ee38e0925e147ecf8c901
Afiq Anuar <afiqaize@gmail.com>
2023-06-19 23:39:41 +0200

    prototype revamped nll plotter

--- ahtt/scripts/plot_nll.py
+++ ahtt/scripts/plot_nll.py
@@ -9,6 +9,8 @@ import numpy as np
 from scipy.interpolate import UnivariateSpline
 import math
 
+from ROOT import TFile, TTree
+
 import glob
 from collections import OrderedDict
 import json
@@ -21,123 +23,119 @@ import matplotlib.colors as mcl
 from drawings import min_g, max_g, epsilon, axes, first, second, get_point
 from desalinator import prepend_if_not_empty, tokenize_to_list, remove_spaces_quotes
 
-def read_nll(directories, onepoi, max_g):
-    nlls = [OrderedDict() for dd in directories]
-    for ii, directory, tag in enumerate(directories):
-        with open("{dcd}/{pnt}_{tag}_nll_{mod}.json".format(dcd = directory, tag = tag, pnt = '_'.join(directory.split('_')[:3]), mod = "one-poi" if onepoi else "g-scan")) as ff:
-            result = json.load(ff)
+def read_nll(points, directories, name, rangex, rangey, kinks, zeropoint):
+    result = []
+    pstr = "__".join(points)
 
-        obs_r = sys.float_info.max
-        obs_g = sys.float_info.max
+    if zeropoint == "minimum":
+        zeros = None
+    else:
+        zeros = []
+        zeropoint = float(zeropoint)
 
-        for sce in ["exp-bkg", "exp-sig", "obs"]:
-            nll = result[sce]
-            nll0 = nll["obs"]["nll0"]
-            dnlls = []
+        for ii, (directory, scenario, tag) in enumerate(directories):
+            files = glob.glob(f"{directory}/{pstr}_{tag}_nll_{scenario}_{name}*.root") # FIXME this will barf if there are more than 1 file with the same param
+            zero = (sys.float_info.max, sys.float_info.max)
 
-            for g, dnll in nll["dnll"].items():
-                if abs(float(g)) > epsilon:
-                    print("WARNING: in directory " + dd + ", first nll for scenario " + sce + " is at g = " + g + " instead of 0\n")
-                nll1 = dnll + nll0
-                break
+            dfile = TFile.Open(files[0])
+            dtree = dfile.Get("limit")
 
-            if sce == "obs":
-                obs_r = nll["obs"]["r"]
-                obs_g = nll["obs"]["g"]
+            for i in dtree:
+                if dtree.quantileExpected >= 0.:
+                    value = getattr(dtree, name)
+                    delta = abs(value - zeropoint)
+                    if delta < zero[-1]:
+                        zero = (dtree.nll, delta)
+            zeros.append(zero)
+            dfile.Close()
 
-            for g, dnll in nll["dnll"].items():
-                gval = float(g)
-                if gval <= max_g:
-                    ispline = -1
-                    for ii, kk in enumerate(kinks):
-                        ispline = ii if kk[0] < gval < kk[1] else ispline
+    for ii, (directory, scenario, tag) in enumerate(directories):
+        originals = []
+        files = glob.glob(f"{directory}/{pstr}_{tag}_nll_{scenario}_{name}*.root")
+        dfile = TFile.Open(files[0])
+        dtree = dfile.Get("limit")
 
-                    if ispline > -1:
-                        spline = UnivariateSpline(np.array(kinks[ispline]),
-                                                  np.array([dd + nll0 - nll1 for gg, dd in nll["dnll"].items() if kinks[ispline][0] <= float(gg) <= kinks[ispline][1]]))
-                        dnlls.append( (gval, spline(gval)) )
-                    else:
-                        dnlls.append( (gval, dnll + nll0 - nll1) )
+        for i in dtree:
+            if dtree.quantileExpected >= 0.:
+                value = getattr(dtree, name)
+                dnll = dtree.deltaNLL if zeros is None else dtree.deltaNLL + dtree.nll0 - zeros[ii][0]
+                dnll *= 2.
+                originals.append((value, dnll))
+        dfile.Close()
+        originals = sorted(originals, key = lambda tup: tup[0])
+        intx = []
+        inty = []
+        dataset = []
 
-            nlls[ii][sce] = dnlls
+        if kinks is not None:
+            for kink in kinks:
+                values = [vv for vv, dd in originals.items() if kink[0] <= vv <= kink[1]]
+                dnlls = [dd for vv, dd in originals.items() if kink[0] <= vv <= kink[1]]
 
-        nlls[ii]["r"] = obs_r
-        nlls[ii]["g"] = obs_g
+                if len(values) > 1 and len(dnlls) == len(values):
+                    spline = UnivariateSpline(np.array(values), np.array(dnlls))
+                    intx.append(value)
+                    inty.append(spline(value))
 
-    return nlls
+        for value, dnll in originals.items():
+            if rangex[0] <= value <= rangex[1]:
+                data = inty[intx.index(value)] if value in intx else dnll
+                if rangey[0] <= data <= rangey[1]:
+                    dataset.append((value, data))
+        result.append(dataset)
+    return result
 
-def draw_nll(oname, directories, labels, onepoi, smooth, kinks, max_g, max_dnll, bestfit, transparent, plotformat):
-    if len(directories) > 3:
-        raise RuntimeError("current plotting code is not meant for more than 3 tags. aborting")
-
-    # FIXME bestfit
-
-    nlls = read_nll(directories, onepoi, kinks, max_g)
-    point = get_point('_'.join(directories[0].split('_')[:3]))
-
+def draw_nll(oname, points, directories, labels, kinks, namelabel, rangex, rangey, zeropoint, transparent, plotformat):
     if not hasattr(draw_nll, "colors"):
-        draw_nll.colors = OrderedDict([
-            (1, ["black"]),
-            (2, ["#cc0033", "#0033cc"]),
-            (3, ["#cc0033", "#0033cc", "black"])
+        draw_nll.settings = OrderedDict([
+            (1, [["black"], ["solid"]]),
+            (2, [["#cc0033", "#0033cc"], ["solid", "dashed"]]),
+            (3, [["black", "#cc0033", "#0033cc"], ["solid", "dashed", "dotted"]]),
+            (4, [["black", "#cc0033", "#0033cc", "#33cc00"], ["solid", "dashed", "dashdot", "dotted"]]),
         ])
-        draw_nll.lines = ["dotted", "dashed", "solid"]
 
-    min_dnll = sys.float_info.max
+    ndir = len(directories)
+    if ndir < 5:
+        colors, styles = draw_nll.settings[ndir]
+    elif ndir < 9:
+        if ndir % 2 == 0:
+            colors = []
+            styles = []
+            for ii in range(ndir / 2):
+                colors.append(draw_nll.settings[ndir / 2][0][ii])
+                colors.append(draw_nll.settings[ndir / 2][0][ii])
+                styles.append("solid")
+                styles.append("dashed")
+        else:
+            raise RuntimeError("cant assign by color/style for uneven counts")
+    else:
+        raise RuntimeError("too many scenarios")
+
     fig, ax = plt.subplots()
     handles = []
-    ymax = max_dnll
+    name, xlabel = namelabel if len(namelabel) > 1 else namelabel + namelabel
+    nlls = read_nll(points, directories, name, rangex, rangey, kinks, zeropoint)
 
     for ii, nll in enumerate(nlls):
-        if ii == 0:
-            for jj, sce in enumerate([("exp-bkg", "Expected"), ("exp-sig", "Signal injected"), ("obs", "Observed")]):
-                handles.append((mln.Line2D([0], [0], color = "black", linestyle = draw_nll.lines[jj], linewidth = 1.5),
-                                sce[1]))
+        color = colors[ii]
+        style = styles[ii]
+        label = labels[ii]
+        handles.append((mln.Line2D([0], [0], color = color, linestyle = style, linewidth = 1.5), label))
+        values = np.array([nn[0] for nn in nll])
+        dnlls = np.array([nn[1] for nn in nll])
+        ax.plot(values, dnlls, color = color, linestyle = style, linewidth = 1.5)
 
-        for jj, sce in enumerate(["exp-bkg", "exp-sig", "obs"]):
-            gs = [nn[0] for nn in nll[sce] if 2. * nn[1] < ymax]
-            dnlls = [2. * nn[1] for nn in nll[sce] if 2. * nn[1] < ymax]
-
-            if min(dnlls) < min_dnll:
-                min_dnll = min(dnlls)
-
-            ax.plot(np.array(gs), np.array(dnlls), color = draw_nll.colors[len(directories)][ii], linestyle = draw_nll.lines[jj], linewidth = 1.5)
-
-            if jj == 0:
-                handles.append((mln.Line2D([0], [0], color = draw_nll.colors[len(directories)][ii], linestyle = draw_nll.lines[-1], linewidth = 1.5),
-                                labels[ii]))
-
-    ymin = math.ceil(math.sqrt(abs(min_dnll))) # ceil of sqrt ~ upper nsigma
-    ymin *= ymin # go back to the dnll
-    ymin = -round(ymin, -2)
-
-    plt.ylim((ymin, 2. * ymax))
-    ax.plot([0., max_g], [ymax, ymax], color = "black", linestyle = 'solid', linewidth = 2)
-    plt.xlabel(axes["coupling"] % point[0], fontsize = 21, loc = "right")
-    plt.ylabel(axes["dnll"] % point[0], fontsize = 21, loc = "top")
+    plt.ylim(rangey)
+    ax.plot(rangex, [rangey[1], rangey[1]], color = "black", linestyle = 'solid', linewidth = 2)
+    plt.xlabel(xlabel, fontsize = 21, loc = "right")
+    plt.ylabel(axes["dnll"] % 'A/H', fontsize = 21, loc = "top")
     ax.margins(x = 0, y = 0)
 
-    lheight = ((2. * ymax) - ymax) / ((2. * ymax) - ymin)
-    lmargin = 0.02
-    lwidth = 1. - (2. * lmargin)
-    dummy = ax.legend([], [],
-	              loc = "upper right", ncol = 1, bbox_to_anchor = (lmargin, 1. - lheight, lwidth, lheight - 0.025),
-                      mode = "expand", borderaxespad = 0., handletextpad = 0.5, fontsize = 21, frameon = False,
-                      title = "Signal point", title_fontsize = 21)
-    ax.add_artist(dummy)
-
-    tags = ax.legend(first(handles[3:]), second(handles[3:]),
-	             loc = "upper right", ncol = 1, bbox_to_anchor = (lmargin, 1. - lheight, 0.5 * lwidth, lheight - 0.025),
+    legend = ax.legend(first(handles), second(handles),
+	             loc = "upper right", ncol = 1, #bbox_to_anchor = ,
                      mode = "expand", borderaxespad = 0., handletextpad = 0.5, fontsize = 21, frameon = False,
                      title = " ", title_fontsize = 21)
-    ax.add_artist(tags)
-
-    scenarii = ax.legend(first(handles[:3]), second(handles[:3]),
-	                 loc = "upper right", ncol = 1, bbox_to_anchor = (lmargin + (0.5 * lwidth), 1. - lheight, 0.5 * lwidth, lheight - 0.025),
-                         mode = "expand", borderaxespad = 0., handletextpad = 0.5, fontsize = 21, frameon = False,
-                         title = " ", title_fontsize = 21)
-    ax.add_artist(scenarii)
-
+    ax.add_artist(legend)
     ax.minorticks_on()
     ax.tick_params(axis = "both", which = "both", direction = "in", bottom = True, top = False, left = True, right = True)
     ax.tick_params(axis = "both", which = "major", width = 1, length = 8, labelsize = 18)
@@ -150,37 +148,55 @@ def draw_nll(oname, directories, labels, onepoi, smooth, kinks, max_g, max_dnll,
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("--point", help = "signal point to plot the pulls of", default = "", required = True, type = remove_spaces_quotes)
-    parser.add_argument("--tag", help = "input tag-output-tag pairs to search. the pairs are semicolon separated, and tags colon-separated, "
-                        "so e.g. when there are 2 tags: 't1:o1;t2:o2...", dest = "itag", default = "", required = False, type = lambda s: tokenize_to_list( remove_spaces_quotes(s), ';' ))
+    parser.add_argument("--point", help = combine_help_messages["--point"], default = "", required = required, type = lambda s: sorted(tokenize_to_list( remove_spaces_quotes(s) )))
+
+    parser.add_argument("--tag", help = "(input tag, scenario, output tag) triplet to search. the pairs are semicolon separated, and tags/scenario colon-separated, "
+                        "so e.g. when there are 2 tags: 't1:s1:o1;t2:s2:o2 . output tag may be empty, in which case input tag is used for it.",
+                        dest = "itag", default = "", required = False, type = lambda s: tokenize_to_list( remove_spaces_quotes(s), token = ';' ))
     parser.add_argument("--plot-tag", help = "extra tag to append to plot names", dest = "ptag", default = "", required = False, type = prepend_if_not_empty)
     parser.add_argument("--odir", help = "output directory to dump plots in", default = ".", required = False, type = remove_spaces_quotes)
-    parser.add_argument("--label", help = "labels to attach on plot for each input tags, semicolon separated", default = "", required = False, type = lambda s: tokenize_to_list(s, ';' ))
-    parser.add_argument("--one-poi", help = "plot pulls obtained with the g-only model", dest = "onepoi", action = "store_true", required = False)
+    parser.add_argument("--label", help = "labels to attach on plot for each tag triplet, semicolon separated", default = "", required = False,
+                        type = lambda s: tokenize_to_list(s, token = ';' ))
     parser.add_argument("--smooth", help = "use spline to smooth kinks up. kinks are given in --kinks", action = "store_true", required = False)
-    parser.add_argument("--kinks", help = "comma separated list of g values to be used by --smooth. every 2 values are treated as min and max of kink range",
+    parser.add_argument("--kinks", help = "comma separated list of values to be used by --smooth. every 2 values are treated as min and max of kink range",
                         default = "", required = False, type = lambda s: None if s == "" else tokenize_to_list( remove_spaces_quotes(s) ) )
-    parser.add_argument("--max-g", help = "max value of g to incude in plot", dest = "max_g", type = float, default = 3., required = False)
-    parser.add_argument("--max-dnll", help = "max value of dnll to incude in plot", dest = "max_dnll", type = float, default = 36., required = False)
-    parser.add_argument("--best-fit", help = "write the observed best fit points on the plot",
-                        dest = "bestfit", action = "store_true", required = False)
+    parser.add_argument("--x-name-label", help = "semicolon-separated parameter name and label on the x-axis. empty label means same as name for NPs.",
+                        dest = "namelabel", type = lambda s: tokenize_to_list( remove_spaces_quotes(s), token = ';'), default = ["g1", ""], required = False)
+    parser.add_argument("--x-range", help = "comma-separated min and max values in the x-axis", dest = "rangex",
+                        type = lambda s: tokenize_to_list( remove_spaces_quotes(s), astype = float), default = [0., 2.], required = False)
+    parser.add_argument("--y-range", help = "comma-separated min and max values in the y-axis", dest = "rangey",
+                        type = lambda s: tokenize_to_list( remove_spaces_quotes(s), astype = float), default = [0., 36.], required = False)
+    parser.add_argument("--zero-point", help = "point to mark as 0 on the 2dNLL axis. can be the 2dNLL minimum, or a given value value on the x axis.", dest = "zeropoint",
+                        default = "minimum", required = False)
 
     parser.add_argument("--opaque-background", help = "make the background white instead of transparent",
                         dest = "transparent", action = "store_false", required = False)
     parser.add_argument("--plot-format", help = "format to save the plots in", default = ".png", dest = "fmt", required = False, type = lambda s: prepend_if_not_empty(s, '.'))
 
     args = parser.parse_args()
+    points = args.point
+    if len(points) != 2:
+        raise RuntimeError("this script is to be used with exactly two A/H points!")
+    pstr = "__".join(points)
+
     if len(args.itag) != len(args.label):
         raise RuntimeError("length of tags isnt the same as labels. aborting")
 
-    if args.kinks is not None:
+    if args.smooth and args.kinks is not None:
         if len(args.kinks) % 2 == 1:
             raise RuntimeError("kinks given don't correspond to list of minmaxes. aborting!")
 
-        args.kinks = [[kinks[ii], kinks[ii + 1]] for ii in range(len(args.kinks) - 1)]
+        args.kinks = [[kinks[ii], kinks[ii + 1]] for ii in range(0, len(args.kinks), 2)]
+    else:
+        args.kinks = None
 
-    dirs = [[args.point + '_' + tag.split(':')[0], tag.split(':')[1] if len(tag.split(':')) > 1 else tag.split(':')[0]] for tag in args.itag]
-    draw_nll("{ooo}/{pnt}_nll_{mod}{tag}{fmt}".format(ooo = args.odir, pnt = args.point, mod = "one-poi" if args.onepoi else "g-scan", tag = args.ptag, fmt = args.fmt),
-             dirs, args.label, args.onepoi, args.smooth, args.kinks, args.max_g, args.max_dnll, args.bestfit, args.transparent, args.fmt)
+    if args.namelabel[1] == "" and args.namelabel[0] in ["g1", "g2"]:
+        args.namelabel[1] = axes["coupling"] % str_point(points[0]) if args.namelabel[0] == "g1" else axes["coupling"] % str_point(points[1])
 
+    dirs = [tag.split(':') for tag in args.itag]
+    dirs = [tag + tag[:1] if len(tag) == 2 else tag for tag in tag]
+    dirs = [[f"{pstr}_{tag[0]}"] + tag[1:] for tag in tag]
+
+    draw_nll(f"{args.odir}/{pstr}_nll_{args.ptag}{args.fmt}",
+             points, dirs, args.label, args.kinks, args.namelabel, args.rangex, args.rangey, args.zeropoint, args.transparent, args.fmt)
     pass
