f553f5250a0d48515cd3addb6f4b57e5a632ecb4
Laurids Jeppe <laurids.jeppe@desy.de>
2023-07-13 18:42:40 +0200

    Added Yukawa model, fudging scripts

---
+++ ahtt/yukawa_fudging/add_yukawa_templates.py
@@ -0,0 +1,111 @@
+import uproot
+import argparse
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument("inputtemplates")
+parser.add_argument("--plotout", default=None)
+args = parser.parse_args()
+
+proc = "TT"
+var_name = "EWK_yukawa"
+
+channels = ["em", "ee", "mm"]
+years = ["2016pre", "2016post", "2017", "2018"]
+
+dyt_up = 0.11
+dyt_down = -0.12
+
+yt_nom = 1.0
+yt_up = yt_nom + dyt_up
+yt_down = yt_nom + dyt_down
+
+templates = {}
+templates_up = {}
+templates_down = {}
+
+with uproot.open(args.inputtemplates) as rfile:
+    for channel in channels:
+        for year in years:
+            cat = channel + "_" + year
+            rcat = rfile[cat]
+            templates[cat] = rcat[proc].values()
+            if proc + "_" + var_name + "Up" in rcat:
+                templates_up[cat] = rcat[proc + "_" + var_name + "Up"].values()
+                templates_down[cat] = rcat[proc + "_" + var_name + "Down"].values()
+
+# We define dyt = yt - 1 and set for the yield:
+# N(EWK + TT) = N(TT) + a * dyt + b * dyt^2
+# where a and b are input templates for the linear and quadratic parts
+#
+# To get a and b, we use the existing Yukawa variation N(TT)_up and N(TT)_down, and set
+#
+# N(TT) + a * dyt_up + b * dyt_up^2 == N_up
+# N(TT) + a * dyt_down + b * dyt_down^2 == N_down
+#
+# where dyt_up = +0.11 and dyt_down = -0.12 are the Yukawa values where the variation was evaluated.
+# We then solve this system of two equations for a and b, which leads to the formula below.
+
+output_templates = {}
+
+for cat in templates.keys():
+
+    dN_up = templates_up[cat] - templates[cat]
+    dN_down = templates_down[cat] - templates[cat]
+
+    denom = dyt_up * dyt_down * (dyt_up - dyt_down)
+
+    a = (dN_down * dyt_up**2 - dN_up * dyt_down**2) / denom
+    b = -(dN_down * dyt_up - dN_up * dyt_down) / denom
+
+    # we need to split a and b into negative and positive parts
+
+    a_pos = np.where(a>=0 , a, 0.)
+    a_neg = np.where(a<=0 , -a, 0.)
+    b_pos = np.where(b>=0 , b, 0.)
+    b_neg = np.where(b<=0 , -b, 0.)
+
+    # Check that this reproduces the input templates
+
+    def predict_yukawa(dyt):
+        return templates[cat] + \
+            dyt * a_pos - dyt * a_neg + \
+            dyt**2 * b_pos - dyt**2 * b_neg
+
+    assert np.all(np.isclose(predict_yukawa(0.), templates[cat])) # nominal
+    assert np.all(np.isclose(predict_yukawa(dyt_up), templates_up[cat])) # up
+    assert np.all(np.isclose(predict_yukawa(dyt_down), templates_down[cat])) # down
+
+    # Add bin edges so that uproot saves it as a TH1D
+    bin_edges = np.arange(len(templates[cat])+1)
+
+    output_templates[cat + "/EWK_quadpos"] = (b_pos, bin_edges)
+    output_templates[cat + "/EWK_quadneg"] = (b_neg, bin_edges)
+    output_templates[cat + "/EWK_linpos"] = (a_pos, bin_edges)
+    output_templates[cat + "/EWK_linneg"] = (a_neg, bin_edges)
+
+if args.plotout is not None:
+    import os
+    import matplotlib.pyplot as plt
+    import mplhep
+
+    if not os.path.isdir(args.plotout):
+        os.makedirs(args.plotout)
+    for name, (hist, edges) in output_templates.items():
+        plt.figure(dpi=200)
+        mplhep.histplot(hist, edges)
+        plt.title(name)
+        plt.savefig(args.plotout + "/" + name.replace("/", "_") + ".png")
+        plt.close()
+
+#oldfile = {}
+#with uproot.open(args.inputtemplates) as rfile:
+#    for key in rfile.keys():
+#        oldfile[key[:-2]] = rfile[key]
+#
+with uproot.update(args.inputtemplates) as rfile:
+    #for key, hist in oldfile.items():
+    #    rfile[key] = hist
+    for key, hist in output_templates.items():
+        rfile[key] = hist
+
---
+++ ahtt/yukawa_fudging/read_yukawa_np.py
@@ -0,0 +1,102 @@
+import ROOT
+import argparse
+import numpy as np
+import os
+import uproot
+import matplotlib.pyplot as plt
+import mplhep
+from scipy.interpolate import interp1d
+
+parser = argparse.ArgumentParser()
+parser.add_argument("rootfile")
+parser.add_argument("inputtemplates")
+args = parser.parse_args()
+
+var_name = "EWK_yukawa"
+category = "em_2018"
+procs = ["TT", "TQ", "TB", "TW", "TTV", "VV", "DY"]
+
+dyt_up = 0.11
+dyt_down = 0.12
+
+yt_nom = 1.0
+yt_up = yt_nom + dyt_up
+yt_down = yt_nom - dyt_down
+
+templates = {}
+templates_up = {}
+templates_down = {}
+
+with uproot.open(args.inputtemplates) as rfile:
+    rcat = rfile[category]
+    for proc in procs:
+        templates[proc] = rcat[proc].values()
+        if proc + "_" + var_name + "Up" in rcat:
+            templates_up[proc] = rcat[proc + "_" + var_name + "Up"].values()
+            templates_down[proc] = rcat[proc + "_" + var_name + "Down"].values()
+
+interpolators = {}
+
+for proc in procs:
+    if proc in templates_up:
+        interpolators[proc] = interp1d(
+            [yt_down, yt_nom, yt_up],
+            [templates_down[proc], templates[proc], templates_up[proc]],
+            axis=0, kind="quadratic", fill_value="extrapolate")
+
+n_bins = len(templates["TT"])
+
+f = ROOT.TFile.Open(args.rootfile)
+w = f.Get("w")
+
+func = w.function("prop_bin" + category)
+x_var = w.var("CMS_th1x")
+y_var = w.var(var_name)
+
+w.var("g1").setVal(0.)
+w.var("g2").setVal(0.)
+
+def get_combine_events(var_val):
+    n_exp = np.empty(n_bins)
+
+    y_var.setVal(var_val)
+    for i in range(n_bins):
+        x_var.setVal(i + 0.5)
+        n_exp[i] = func.getVal()
+
+    return n_exp
+
+def get_naive_events(yt):
+    n_exp = np.zeros(n_bins)
+
+    for proc in procs:
+        if proc in interpolators:
+            n_exp += interpolators[proc](yt)
+        else:
+            n_exp += templates[proc]
+
+    return n_exp
+
+fig, (ax1, ax2) = plt.subplots(dpi=200, nrows=2)
+
+colors = plt.get_cmap("tab10")
+edges = np.arange(n_bins+1)
+
+for i, sigmayt in enumerate([-2, -1, -0.5, 0, 0.5, 1, 2]):
+    yt = yt_nom + sigmayt * dyt_up if sigmayt >= 0 else yt_nom + sigmayt * dyt_down
+    n_combine = get_combine_events(sigmayt)
+    n_naive = get_naive_events(yt)
+
+    print("Agreement for g = {g}: {a}".format(g=yt, a=np.all(np.isclose(n_combine, n_naive))))
+
+    mplhep.histplot(n_combine, edges, color=colors(i), linestyle="dotted", linewidth=1, edges=False, ax=ax1, label="yt = {g}".format(g=yt))
+    mplhep.histplot(n_naive, edges, color=colors(i), linestyle="dashed", linewidth=1, edges=False, ax=ax1)
+
+    mplhep.histplot(n_combine-n_naive, edges, color=colors(i), linewidth=1, edges=False, ax=ax2)
+
+ax1.set_yscale("log")
+ax1.autoscale()
+ax2.autoscale()
+ax1.legend()
+plt.savefig("yukawayields.png")
+plt.close()
