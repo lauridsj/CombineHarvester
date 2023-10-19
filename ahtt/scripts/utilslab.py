212074a5082c8a8faccfc43d271d661ccaebf5b2
Afiq Anuar <afiqaize@gmail.com>
2023-10-19 23:44:49 +0200

    somehow it has dyt

--- ahtt/scripts/utilslab.py
+++ ahtt/scripts/utilslab.py
@@ -37,7 +37,7 @@ def input_bkg(background, channels):
 
     backgrounds = []
     if any(cc in channels for cc in ["ee", "em", "mm"]):
-        backgrounds.append(input_base + "templates_ULFR2/dyscales_231019/ll/bkg_ll_3D-33_rate_mtuX_pca_ewk.root")
+        backgrounds.append(input_base + "templates_ULFR2/dyscales_231019/ll/bkg_ll_3D-33_rate_mtuX_pca_ewk_dyt.root")
     if any(cc in channels for cc in ["e3j", "e4pj", "m3j", "m4pj"]):
         backgrounds.append(input_base + "templates_ULFR2/dyscales_231019/lj/templates_lj_bkg_rate_mtuX_pca_ewk.root")
 
