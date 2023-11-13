099636b259803fe42b78933997b3aa162777a945
Afiq Anuar <afiqaize@gmail.com>
2023-11-13 10:45:24 +0100

    covmat doesnt have any relevant info here

--- ahtt/scripts/plot_prepost.py
+++ ahtt/scripts/plot_prepost.py
@@ -417,8 +417,8 @@ with uproot.open(args.ifile) as f:
             signals[("Total", None, None)] = total
         datavalues = directory["data"].values()[1][:len(centers)]
         total = directory["total"].to_hist()[:len(centers)]
-        covariance = directory["total_covar"].to_hist()[:len(centers), :len(centers)]
-        total = add_covariance(total, covariance)
+        #covariance = directory["total_covar"].to_hist()[:len(centers), :len(centers)]
+        #total = add_covariance(total, covariance)
         datahist_errors = np.array([directory["data"].errors("low")[1], directory["data"].errors("high")[1]])[:, :len(centers)]
 
         kwargs = {
