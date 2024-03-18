b28232cc2a9f4b2780c31b829a131131df6385d9
Afiq Anuar <afiqaize@gmail.com>
2024-03-18 16:09:28 +0100

    flat etat

--- ahtt/scripts/make_datacard.py
+++ ahtt/scripts/make_datacard.py
@@ -799,7 +799,7 @@ def write_datacard(oname, cpn, years, sigpnt, injsig, assig, drops, keeps, mcsta
             cc = os.path.basename(tt).replace("ahtt_", "").replace(".txt", "")
             groups[cc]["norm"].append("CMS_EtaT_norm_13TeV")
             with open(tt, 'a') as txt:
-                txt.write("\nCMS_EtaT_norm_13TeV param 0 1")
+                #txt.write("\nCMS_EtaT_norm_13TeV param 0 1")
                 txt.write("\nCMS_EtaT_norm_13TeV rateParam * EtaT 0 [-5,5]")
                 txt.write("\n")
 
