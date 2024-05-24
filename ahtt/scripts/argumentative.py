5da32fe015293c3970ec69c6e87fb3e3e9eda675
Afiq Anuar <afiqaize@gmail.com>
2024-05-24 20:43:21 +0200

    unused nll-full-range follows nll-interval

--- ahtt/scripts/argumentative.py
+++ ahtt/scripts/argumentative.py
@@ -157,14 +157,17 @@ def parse_args(parser):
     if args.otag == "":
         args.otag = args.tag
     if hasattr(args, 'nllfullrange') and hasattr(args, 'nllparam'):
-        if not hasattr(args, 'nllnjob'):
-            args.nllnjob = [10] * len(args.nllparam)
-        result = []
-        for minmax, njob, in zip(args.nllfullrange, args.nllnjob):
-            mm = minmax.split(',')
-            each = list(np.linspace(float(mm[0]), float(mm[1]), num = njob))
-            each = ["{0},{1}".format(each[ii], each[ii + 1]) for ii in range(len(each) - 1)] if len(each) > 1 else ["{0},{0}".format(each[0])] if len(each) == 1 else []
-            result.append(each)
-        args.nllfullrange = itertools.product(*result)
+        if args.nllfullrange != []:
+            if not hasattr(args, 'nllnjob'):
+                args.nllnjob = [10] * len(args.nllparam)
+            result = []
+            for minmax, njob, in zip(args.nllfullrange, args.nllnjob):
+                mm = minmax.split(',')
+                each = list(np.linspace(float(mm[0]), float(mm[1]), num = njob))
+                each = ["{0},{1}".format(each[ii], each[ii + 1]) for ii in range(len(each) - 1)] if len(each) > 1 else ["{0},{0}".format(each[0])] if len(each) == 1 else []
+                result.append(each)
+            args.nllfullrange = itertools.product(*result)
+        else:
+            args.nllfullrange = [args.nllinterval]
     rng_seeder(args.seed)
     return args
